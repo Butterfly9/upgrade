a70d3c2c7cb3fa10e15467475b8352c8c976d9bc
Khusharth Patani <khusharth19@gmail.com>
2019-03-22 05:37:27 +0530

    Changed update_version.py file

--- update_version.py
+++ update_version.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import os
+import requests
+
+image_url = "https://raw.githubusercontent.com/Butterfly9/upgrade/master/make_pdf.py"
+  
+r = requests.get(image_url) # create HTTP response object 
+  
+with open("make_pdf.py",'wb') as f: 
+    f.write(r.content)
+
+print("file downloaded")
+os.remove("../website/make_pdf.py")
+print("File deleted")
+os.rename("make_pdf.py", "../website/make_pdf.py")
+print("File copied")
+
